864bd2733656bb18f597b040e1a714836367370b
JericoFX <blackjordison@gmail.com>
2023-01-29 19:42:28 -0300

    Example

--- src/Playerss/server.ts
+++ src/Playerss/server.ts
@@ -1,5 +1,13 @@
 import * as alt from 'alt-server';
 import { QVCore } from '../qv-core/server';
 alt.on('jerico', async (player) => {
-    console.log(QVCore.Functions.GetPlayerById(player.id));
+    const Data = player.getSyncedMeta('metadata');
+    console.log(Data);
+    Data.metadata.bloodtype = 'ENTYY';
+    //----Update Value
+    player.setSyncedMeta('metadata', Data);
+    const updated = player.getSyncedMeta('metadata');
+    console.log(updated);
+
+    // console.log(QVCore.Functions.GetPlayerById(player.id));
 });
