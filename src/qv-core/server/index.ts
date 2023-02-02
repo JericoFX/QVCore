7464fd0bcd0413d5011f6e49275ebc4b1a874421
JericoFX <blackjordison@gmail.com>
2023-02-01 22:55:06 -0300

    Some Fixes

--- src/qv-core/server/index.ts
+++ src/qv-core/server/index.ts
@@ -3,15 +3,22 @@ import * as alt from 'alt-server';
 import * as chat from 'chat';
 import Database from '@stuyk/ezmongodb';
 import { GetPlayerReady } from './modules/Core/Core';
-////import { QVCore } from './exports';
-//import Player from './modules/Player/Player';
-//
+import { allowDamageEventsForNonNetworkedObjects } from 'natives';
+
 const url = 'mongodb://localhost:27017';
 const dbName = 'jericore';
 const collections = ['accounts', 'characters', 'vehicles'];
-//
+
 (async () => {
-    const connect = await Database.init(url, dbName, collections);
+    alt.log('DATABASE');
+    try {
+        const connect = await Database.init(url, dbName, collections);
+        if (!connect) {
+            alt.log('Concha negra');
+        }
+    } catch (error) {
+        alt.logError(error);
+    }
     await Database.createSearchIndex('license', 'accounts');
 })();
 //alt.log('SE LLAMO');
@@ -19,9 +26,9 @@ alt.on('playerConnect', async (player) => {
     player.setSyncedMeta('license', 'JERICOFXX');
     player.setSyncedMeta('citizenid', 'HI');
     GetPlayerReady(player);
-    setTimeout(() => {
-        player.Login();
-    }, 1500);
+
+    player.Login();
+
     //await Jerico.Login();
     player.spawn(33, 0, 70);
     player.model = 'mp_m_freemode_01';
@@ -30,3 +37,12 @@ alt.on('playerConnect', async (player) => {
 chat.registerCmd('g', (player) => {
     alt.emit('jerico', player);
 });
+
+setInterval(async () => {
+    alt.Player.all.forEach(async (item) => {
+        item.Save();
+        alt.log(item.jobName);
+        item.jobName = 'police';
+        alt.log(item.jobName);
+    });
+}, 10000);
--- src/qv-core/server/modules/Core/Core.ts
+++ src/qv-core/server/modules/Core/Core.ts
@@ -3,346 +3,311 @@ import * as alt from 'alt-server';
 //import * as chat from 'chat';
 import ShortUniqueId from 'short-unique-id';
 import Database from '@stuyk/ezmongodb';
-import {Jobs} from '../../../shared/jobs';
-import {Config} from '../../../shared/config';
+import { Jobs } from '../../../shared/jobs';
+import { Config } from '../../../shared/config';
 
 declare module 'alt-server' {
-	interface Player {
-		citizenid: string;
-		license: string;
-		money: Object;
-		charinfo: Object;
-		metadata: Object;
-		job: Object;
-		gang: Object;
-
-		get jobName(): string;
-	}
+    interface Player {
+        citizenid: string;
+        license: string;
+        money: Object;
+        charinfo: Object;
+        metadata: Object;
+        job: Object;
+        gang: Object;
+        position: Object;
+        get jobName(): string;
+    }
 }
 
 let Players = {};
 
 function ReturnDefaultData() {
-	return {
-		_id: '',
-		license: 'JERICOFXX',
-		citizenid: GenerateCitizenID(),
-		money: {},
-		charinfo: {
-			firstname: '',
-			lastname: '',
-			birthdate: '',
-			gender: 0,
-			backstory: '',
-			nationality: '',
-		},
-		metadata: {
-			hunger: 100,
-			thirst: 100,
-			stress: 0,
-			isDead: false,
-			inlaststand: false,
-			armor: 0,
-			isHandcuffed: false,
-			tracker: false,
-			inJail: false,
-			jailItems: [],
-			status: {},
-			phone: {},
-			fitbit: {},
-			commandbinds: {},
-			bloodtype: {},
-			dealerrep: 0,
-			craftingrep: 0,
-			attachmentcraftingrep: 0,
-			currentapartment: null,
-			jobRep: {
-				tow: 0,
-				trucker: 0,
-				taxi: 0,
-				hotdog: 0,
-			},
-			callSign: 'NO CALLSIGN',
-			licences: {
-				driver: true,
-				business: false,
-				weapon: false,
-			},
-			inside: {
-				house: null,
-				apartment: {
-					apartmentType: null,
-					apartmentId: null,
-				},
-			},
-		},
-		job: {
-			label: 'Civilian',
-			type: 'none',
-			isBoss: false,
-			onDuty: false,
-			grade: {
-				name: 'Freelancer',
-				level: 0,
-				payment: 10,
-				isboss: true,
-			},
-		},
-		gang: {
-			name: 'none',
-			level: 0,
-			isBoss: false,
-		},
-	};
+    return {
+        license: 'JERICOFXX',
+        citizenid: GenerateCitizenID(),
+        money: {},
+        charinfo: {
+            firstname: 'Jerico',
+            lastname: 'Default',
+            birthdate: '',
+            gender: 0,
+            backstory: '',
+            nationality: '',
+        },
+        metadata: {
+            hunger: 100,
+            thirst: 100,
+            stress: 0,
+            isDead: false,
+            inlaststand: false,
+            armor: 0,
+            isHandcuffed: false,
+            tracker: false,
+            inJail: false,
+            jailItems: [],
+            status: {},
+            phone: {},
+            fitbit: {},
+            commandbinds: {},
+            bloodtype: {},
+            dealerrep: 0,
+            craftingrep: 0,
+            attachmentcraftingrep: 0,
+            currentapartment: null,
+            jobRep: {
+                tow: 0,
+                trucker: 0,
+                taxi: 0,
+                hotdog: 0,
+            },
+            callSign: 'NO CALLSIGN',
+            licences: {
+                driver: true,
+                business: false,
+                weapon: false,
+            },
+            inside: {
+                house: null,
+                apartment: {
+                    apartmentType: null,
+                    apartmentId: null,
+                },
+            },
+        },
+        job: {
+            label: 'Civilian',
+            type: 'none',
+            isBoss: false,
+            onDuty: false,
+            grade: {
+                name: 'Freelancer',
+                level: 0,
+                payment: 10,
+                isboss: true,
+            },
+        },
+        gang: {
+            name: 'none',
+            level: 0,
+            isBoss: false,
+        },
+    };
 }
 // const Information = {
 
 // };
 
 export function GetPlayerReady(player) {
-	if (!player && !player.valid) {
-		alt.logError(`[CORE] Player not valid`);
-		return;
-	}
-	player.license = 'JERICOFXX';
-	alt.Player.prototype.citizenid = '';
-	alt.Player.prototype.money = {};
-	alt.Player.prototype.charinfo = {};
-	alt.Player.prototype.metadata = {};
-	alt.Player.prototype.job = {};
-	alt.Player.prototype.gang = {};
-	ExtendPrototype(player);
+    alt.log('HIIII');
+    if (!player && !player.valid) {
+        alt.logError(`[CORE] Player not valid`);
+        return;
+    }
+    player.license = 'JERICOFXX';
+    if (
+        typeof alt.Player.prototype.citizenid !== 'string' &&
+        typeof alt.Player.prototype.money !== 'object' &&
+        typeof alt.Player.prototype.charinfo !== 'object' &&
+        typeof alt.Player.prototype.metadata !== 'object' &&
+        typeof alt.Player.prototype.job !== 'object' &&
+        typeof alt.Player.prototype.gang !== 'object'
+    ) {
+        alt.log('JELOS');
+        alt.Player.prototype.citizenid = '';
+        alt.Player.prototype.money = {};
+        alt.Player.prototype.charinfo = {};
+        alt.Player.prototype.metadata = {};
+        alt.Player.prototype.job = {};
+        alt.Player.prototype.gang = {};
+        ExtendPrototype(player);
+    }
 
-	player.Login = async () => {
-		if (!player && !player.valid) {
-			alt.logError(`[CORE] Player not valid`);
-			return;
-		}
-		const exist = await Database.fetchWithSearch(player.license, 'accounts');
-		if (exist[0] === undefined) {
-			alt.log('NEW PLAYER');
-			return player.SetData(true, ReturnDefaultData());
-		}
-		alt.log('PLAYER EXISTED');
-		return player.SetData(false, exist[0]);
-	};
+    player.Login = async () => {
+        if (!player && !player.valid) {
+            alt.logError(`[CORE] Player not valid`);
+            return;
+        }
+        const exist = await Database.fetchWithSearch(player.license, 'accounts');
+        if (exist[0] === undefined) {
+            alt.log('NEW PLAYER');
+            return player.SetData(true, ReturnDefaultData());
+        }
+        alt.log('PLAYER EXISTED');
+        player.spawn(exist[0].pos);
+        return player.SetData(false, exist[0]);
+    };
 
-	player.SetData = async (newPayer, newData) => {
-		if (!player && !player.valid) {
-			alt.logError(`[CORE] Player not valid`);
-		}
-		const Dkey = {...newData};
-		if (newPayer) {
-			if (newData.firstname !== '') {
-				newData.job = Jobs['unemployed'];
-				newData.money = {...Config.money};
-				player.setSyncedMeta('charinfo', {...newData});
-			}
-		}
-		for (const [key, value] of Object.entries(newData)) {
-			player.setSyncedMeta(key, newData[key]);
-			player[key] = newData[key];
-		}
-		Players[player.id] = newData;
-		return player.Save();
-	};
-	player.Test = () => {
-		alt.log(player.charinfo);
-		return alt.log('HIIII MIS CHIQUIS');
-	};
+    player.SetData = async (newPayer, newData) => {
+        if (!player && !player.valid) {
+            alt.logError(`[CORE] Player not valid`);
+        }
+        const Dkey = { ...newData };
+        if (newPayer) {
+            if (newData.firstname !== '') {
+                newData.job = Jobs['unemployed'];
+                newData.money = { ...Config.money };
+                player.setSyncedMeta('charinfo', { ...newData });
+                player.spawn(0, 0, 70);
+            }
+        }
+        for (const [key, value] of Object.entries(newData)) {
+            player.setSyncedMeta(key, newData[key]);
+            player[key] = newData[key];
+        }
 
-	player.GetData = () => {
-		return Players[player.id];
-	};
-	player.Save = async () => {
-		const exist = await Database.fetchWithSearch(player.license, 'accounts');
-		const Data = player.GetData();
-		if (!exist[0] === undefined) {
-			await Database.updatePartialData(exist._id, {...Data}, 'accounts');
-		} else {
-			const Jerico = await Database.insertData(Data, 'accounts', false);
-		}
-	};
+        Players[player.id] = newData;
+        return player.Save();
+    };
+    player.Test = () => {
+        alt.log(player.charinfo);
+        return alt.log('HIIII MIS CHIQUIS');
+    };
+
+    player.GetData = () => {
+        return Players[player.id];
+    };
+    player.Save = async () => {
+        alt.log('Player Savedd');
+        const exist = await Database.fetchWithSearch(player.license, 'accounts');
+        let Data = player.GetData();
+        Data.pos = player.pos;
+        if (exist.length > 0) {
+            await Database.updatePartialData(exist[0]._id, { ...Data }, 'accounts');
+            alt.log(`[CORE] ~g~Player ${player.fullName} Saved~g~`);
+        } else {
+            const Jerico = await Database.insertData(Data, 'accounts', false);
+        }
+        return 'SALVADO';
+    };
 }
 
 function GenerateCitizenID() {
-	const uid = new ShortUniqueId({
-		dictionary: 'alphanum_upper',
-		length: 6,
-	});
-	return uid();
+    const uid = new ShortUniqueId({
+        dictionary: 'alphanum_upper',
+        length: 6,
+    });
+    return uid();
 }
 
 function ExtendPrototype(player) {
-	Object.defineProperty(alt.Player.prototype, 'jobName', {
-		/**
-		 * Get the current job, so no more QVCore.Functions.GetPlayer(player.id)
-		 * Now is just player.jobName
-		 * @returns
-		 */
+    Object.defineProperty(alt.Player.prototype, 'jobName', {
+        /**
+         * Get the current job, so no more QVCore.Functions.GetPlayer(player.id)
+         * Now is just player.jobName
+         * @returns
+         */
 
-		get: () => {
-			return player.job.label;
-		},
+        get: () => {
+            return player.job.label;
+        },
 
-		/**
-		 * The same way now you can set the job with:
-		 * player.jobName = "police"
-		 * @returns
-		 */
-		set: (v: string) => {
-			if (!Jobs[v]) {
-				alt.logError(`[CORE] No Job with the name ${v} detected`);
-				return;
-			}
-			player.job = Jobs[v];
-			player.grade = Jobs[v].grade[0];
-		},
-		writable: true,
-	});
+        /**
+         * The same way now you can set the job with:
+         * player.jobName = "police"
+         * @returns
+         */
+        set: (v: string) => {
+            if (!Jobs[v]) {
+                alt.logError(`[CORE] No Job with the name ${v} detected`);
+                return;
+            }
+            player.job = Jobs[v];
+            player.grade = Jobs[v].grades[0];
+        },
+    });
 
-	/**
-	 * Get the current job type
-	 * Now is just player.jobType print("none" or "leo")
-	 * @returns
-	 */
-	Object.defineProperty(alt.Player.prototype, 'jobType', {
-		get: () => {
-			return player.job.type;
-		},
-	});
+    /**
+     * Get the current job type
+     * Now is just player.jobType print("none" or "leo")
+     * @returns
+     */
+    Object.defineProperty(alt.Player.prototype, 'jobType', {
+        get: () => {
+            return player.job.type;
+        },
+    });
 
-	Object.defineProperty(alt.Player.prototype, 'jobGrade', {
-		/**
-		 * Get the current job, so no more QVCore.Functions.GetPlayer(player.id)
-		 * Now is just player.jobGrade
-		 * @returns
-		 */
+    Object.defineProperty(alt.Player.prototype, 'jobGrade', {
+        /**
+         * Get the current job, so no more QVCore.Functions.GetPlayer(player.id)
+         * Now is just player.jobGrade
+         * @returns
+         */
 
-		get: () => {
-			return Jobs[player.job.name].grade.label;
-		},
+        get: () => {
+            return Jobs[player.job.name].grades.label;
+        },
 
-		/**
-		 * The same way now you can set the job with:
-		 * player.jobName = "police"
-		 * @returns
-		 */
-		set: (v: number) => {
-			if (!Jobs[player.job.name].grade[v]) {
-				alt.logError(`[CORE] No Grade with the number ${v} detected`);
-				return;
-			}
-			//            player.job = Jobs[v];
-			player.grade = Jobs[player.job.name].grade[v];
-		},
-		writable: true,
-	});
+        /**
+         * The same way now you can set the job with:
+         * player.jobName = "police"
+         * @returns
+         */
+        set: (v: number) => {
+            if (!Jobs[player.job.name].grades[v]) {
+                alt.logError(`[CORE] No Grade with the number ${v} detected`);
+                return;
+            }
+            //            player.job = Jobs[v];
+            player.grade = Jobs[player.job.name].grades[v];
+        },
+    });
 
-	/**
-	 * Getter: Return if is boss or not
-	 */
-	Object.defineProperty(alt.Player.prototype, 'isBoss', {
-		get: () => {
-			return player.job.grade.isboss || false;
-		},
-	});
+    /**
+     * Getter: Return if is boss or not
+     */
+    Object.defineProperty(alt.Player.prototype, 'isBoss', {
+        get: () => {
+            return player.job.grades.isboss || false;
+        },
+    });
 
-	/**
-	 * Getter: Return if the player is on duty or not
-	 * Setter: Set the value of OnDuty
-	 */
-	Object.defineProperty(alt.Player.prototype, 'onDuty', {
-		get: () => {
-			return player.job.defaultDuty;
-		},
-		set: (v: boolean) => {
-			if (typeof v !== 'boolean') {
-				alt.logError(`[CORE] onDuty expect a boolean, you passed ${typeof v}`);
-			}
-			player.job.defaultDuty = v;
-		},
-	});
+    /**
+     * Getter: Return if the player is on duty or not
+     * Setter: Set the value of OnDuty
+     */
+    Object.defineProperty(alt.Player.prototype, 'onDuty', {
+        get: () => {
+            return player.job.defaultDuty;
+        },
+        set: (v: boolean) => {
+            if (typeof v !== 'boolean') {
+                alt.logError(`[CORE] onDuty expect a boolean, you passed ${typeof v}`);
+            }
+            player.job.defaultDuty = v;
+        },
+    });
 
-	/**
-	 * Getter: Return the firstname of the player
-	 */
-	Object.defineProperty(alt.Player.prototype, 'firstname', {
-		get: () => {
-			return player.charinfo.firstname;
-		},
-	});
+    /**
+     * Getter: Return the firstname of the player
+     */
+    Object.defineProperty(alt.Player.prototype, 'firstname', {
+        get: () => {
+            return player.charinfo.firstname;
+        },
+    });
 
-	/**
-	 * Getter: Return the firstname of the player
-	 */
-	Object.defineProperty(alt.Player.prototype, 'lastname', {
-		get: () => {
-			return player.charinfo.lastname;
-		},
-	});
-	Object.defineProperty(alt.Player.prototype, 'fullname', {
-		get: () => {
-			return `${player.charinfo.firstname} ${player.charinfo.lastname}`;
-		},
-	});
+    /**
+     * Getter: Return the firstname of the player
+     */
+    Object.defineProperty(alt.Player.prototype, 'lastname', {
+        get: () => {
+            return player.charinfo.lastname;
+        },
+    });
+    Object.defineProperty(alt.Player.prototype, 'fullName', {
+        get: () => {
+            return `${player.charinfo.firstname} ${player.charinfo.lastname}`;
+        },
+    });
 
-	/**
-	 * Getter: Return the gender of the player
-	 */
-	Object.defineProperty(alt.Player.prototype, 'gender', {
-		get: () => {
-			return player.charinfo.gender || 0;
-		},
-	});
+    /**
+     * Getter: Return the gender of the player
+     */
+    Object.defineProperty(alt.Player.prototype, 'gender', {
+        get: () => {
+            return player.charinfo.gender || 0;
+        },
+    });
 }
-// get jobName() {
-//     return this.job.name;
-// },
-// get jobLabel() {
-//     return this.job.label;
-// },
-// get jobType() {
-//     return this.job.type;
-// },
-// get IsBoss() {
-//     return this.job.isBoss;
-// },
-// get jobGradeLevel() {
-//     return this.job.grade;
-// },
-// get gangName() {
-//     return this.gang.name;
-// },
-// get gangLevel() {
-//     return this.gang.level;
-// },
-// get gangBoss() {
-//     return this.gang.isBoss;
-// },
-// get firstName() {
-//     return this.charinfo.firstname;
-// },
-// get lastName() {
-//     return this.charinfo.lastname;
-// },
-// get fullName() {
-//     return `${this.charinfo.firstname} ${this.charinfo.lastname}`;
-// },
-// get gender() {
-//     return this.charinfo.gender;
-// },
-// set jobName(v: string) {
-//     if (!Jobs[v.toString()] || typeof v !== 'string') {
-//         alt.logError(`[CORE] No job with the name ${v} Detected`);
-//         return;
-//     }
-//     this.job = Jobs[v];
-// },
-// set jobGrade(v: number) {
-//     if (!Jobs[this.jobName.toString()].grade[v] || typeof v !== 'number') {
-//         alt.logError(`[CORE] No Grade Detected returning`);
-//         return;
-//     }
-//     this.job.grade = Jobs[this.jobName.toString()].grade[v];
-// },
